31a92898876da541011c78465c0e44fed3d79bdd
tarik817 <tarik817@gmail.com>
2018-09-29 19:25:53 +0300

    Fix typo.

--- public/index.php
+++ public/index.php
@@ -4,8 +4,7 @@ require("../vendor/autoload.php");
 
 ini_set('xdebug.max_nesting_level', 500);
 
-$settings = require_once ('../data/setting.php');
+$settings = require_once('../data/settings.php');
 
 $app = new \App\App($settings);
 $app->run();
-
